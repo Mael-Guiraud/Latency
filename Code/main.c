76d4d63822ac60075ac98ab5a68cff1c757836da
yann <yann.strozecki@uvsq.fr>
2016-06-10 08:28:00 +0200

    merge

--- Code/main.c
+++ Code/main.c
@@ -5,7 +5,7 @@ int main(){
 	gettimeofday(&trecv,NULL);
 	srandom(trecv.tv_usec);
 	//0 pour avoir des alea partout, 1 pour des 0 vers feuilles, 2 pour des0 vers sources
-	//simulation(2);
+	simulation(2);
 	tests();
 	//algo_bruteforce(19500);
 	return 0;
