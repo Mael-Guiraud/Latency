4cb929c76c4223befa8f3c1d0fac94d04e49e969
Maël Guiraud <maelguiraud@hotmail.fr>
2021-04-05 14:04:50 +0200

    modfis average worst + load

--- Code Article/Sources/main.c
+++ Code Article/Sources/main.c
@@ -88,12 +88,12 @@ int main()
 	
 
 	//Test stochastique vs pmls
-	//stochastic_vs_PMLS(8,2500,700,1000, 12000);
+	stochastic_vs_PMLS(8,2500,22000,1000, 12000);
 
 
 
 	//Test distrib routes departs
-	distrib_margins_departs(8, 2500,21052,5000, 1000, 21052,1000);
+	//distrib_margins_departs(8, 2500,21052,5000, 1000, 21052,1000);
 
 	//Bout de code pour tester la stabilisation de la marge necessaire quand on simule plusieurs periodes en stochastique
 
